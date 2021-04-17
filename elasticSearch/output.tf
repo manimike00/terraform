1aa54614e99fb964eddf2acfe3993b064a7ccdb7
manimike00 <manimike00@gmail.com>
2021-04-18 00:28:18 +0530

    rm domain policy outputs

--- elasticSearch/output.tf
+++ elasticSearch/output.tf
@@ -14,12 +14,12 @@ output "domain_endpoint" {
   description = "Domain-specific endpoint used to submit index, search, and data upload requests"
 }
 
-output "elasticsearch_user_iam_role_name" {
-  value       = join(",", aws_iam_role.elasticsearch_user.*.name)
-  description = "The name of the IAM role to allow access to Elasticsearch cluster"
-}
-
-output "elasticsearch_user_iam_role_arn" {
-  value       = join(",", aws_iam_role.elasticsearch_user.*.arn)
-  description = "The ARN of the IAM role to allow access to Elasticsearch cluster"
-}
+//output "elasticsearch_user_iam_role_name" {
+//  value       = join(",", aws_iam_role.elasticsearch_user.*.name)
+//  description = "The name of the IAM role to allow access to Elasticsearch cluster"
+//}
+//
+//output "elasticsearch_user_iam_role_arn" {
+//  value       = join(",", aws_iam_role.elasticsearch_user.*.arn)
+//  description = "The ARN of the IAM role to allow access to Elasticsearch cluster"
+//}
