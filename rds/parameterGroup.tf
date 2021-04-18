c90251ef02f989d6873e2e1c51f2543e5712dc9e
manimike00 <manimike00@gmail.com>
2021-04-18 14:30:54 +0530

    rm character_set_server in parameter-grp

--- rds/parameterGroup.tf
+++ rds/parameterGroup.tf
@@ -1,14 +1,5 @@
 resource "aws_db_parameter_group" "dbParameterGroup" {
-  name   = "${var.name}-rds-pg"
-  family = var.family
-
-  parameter {
-    name  = "character_set_server"
-    value = "utf8"
-  }
-
-  parameter {
-    name  = "character_set_client"
-    value = "utf8"
-  }
+  name        = "${var.name}-rds-pg"
+  family      = var.family
+  description = ""${var.name} RDS Parameter Group""
 }
